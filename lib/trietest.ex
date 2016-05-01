e5628c4ed06f7d6eae28df2571a8dd3b45a79209
Bryce Kerley <bkerley@brycekerley.net>
2016-04-30 20:23:58 -0400

    blah

--- lib/trietest.ex
+++ lib/trietest.ex
@@ -1,2 +1,43 @@
 defmodule Trietest do
+  defstruct end_word: false, children: %{}
+
+  def add_word(%Trietest{} = t, some_word) when is_binary(some_word) do
+    add_word(t, String.to_char_list(some_word))
+  end
+
+  def add_word(%Trietest{} = t, [] = _end_of_word) do
+    struct(t, end_word: true)
+  end
+
+  def add_word(%Trietest{children: children} = here,
+               [first_char | remain] = _word) do
+    found_child = children[first_char]
+    subtrie = case found_child do
+                nil -> %Trietest{}
+                actual_child -> actual_child
+              end
+
+    updated_subtrie = add_word(subtrie, remain)
+    updated_children = Map.put(children, first_char, updated_subtrie)
+    struct(here, children: updated_children)
+  end
+
+  def include_word(%Trietest{} = t, some_word) when is_binary(some_word) do
+    include_word(t, String.to_char_list(some_word))
+  end
+
+  def include_word(%Trietest{end_word: end_word}, [] = _end_of_word) do
+    end_word
+  end
+
+  def include_word(%Trietest{children: children},
+                   [first_char | remain] = _word) do
+
+    found_child = children[first_char]
+    case found_child do
+      nil -> false
+      some_trie ->
+        include_word(some_trie, remain)
+    end
+  end
 end
--- test/trietest_test.exs
+++ test/trietest_test.exs
@@ -2,7 +2,55 @@ defmodule TrietestTest do
   use ExUnit.Case
   doctest Trietest
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    {:ok, words: ~w{add addition adam additive ada ayyy lmao}}
+  end
+
+  test "single character" do
+    trie = %Trietest{}
+    |> Trietest.add_word('a')
+
+    assert %Trietest{children: %{97 => %Trietest{end_word: true}}} == trie
+
+    assert Trietest.include_word(trie, 'a')
+
+    refute Trietest.include_word(trie, 'x')
+    refute Trietest.include_word(trie, 'aaa')
+  end
+
+  test "two words, different prefix" do
+    trie = %Trietest{}
+    |> Trietest.add_word('ab')
+    |> Trietest.add_word('aa')
+
+    assert %Trietest{
+      children: %{97 =>
+                   %Trietest{
+                     children: %{97 => %Trietest{end_word: true},
+                                 98 => %Trietest{end_word: true}},
+                     end_word: false}},
+      end_word: false} == trie
+
+    same_trie = %Trietest{}
+    |> Trietest.add_word('aa')
+    |> Trietest.add_word('ab')
+
+    assert trie == same_trie
+
+    assert Trietest.include_word(trie, 'ab')
+    assert Trietest.include_word(trie, 'aa')
+
+    refute Trietest.include_word(trie, 'a')
+  end
+
+  test "trie", %{words: words} do
+    trie = words
+    |> Enum.reduce(%Trietest{}, fn(w, t) -> Trietest.add_word(t, w) end)
+
+    words
+    |> Enum.each(fn(w) ->
+      assert(Trietest.include_word(trie, w),
+             "Expected to include #{w}")
+    end)
   end
 end
